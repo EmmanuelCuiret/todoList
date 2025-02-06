6c0639b0d7f4fdf4e6c54e24087db716af73bc20
emmanuelcuiret <e.cuiret@gmail.com>
2025-02-06 21:53:56 +0100

    update

--- vite-project/src/components/FooterButtons.jsx
+++ vite-project/src/components/FooterButtons.jsx
@@ -5,21 +5,23 @@ const FooterButtons = ({ handleShowAllTasks, handleShowDoneTasks, handleShowTodo
   const { translations, switchLanguage } = useLanguage(); // Récupère les traductions et la fonction de changement de langue
 
   return (
-    <div className="footer-buttons">
-      <button onClick={handleShowTodoTasks} className={activeButton === "todo" ? "active-button" : ""} title="Show tasks to do">
-        {translations.showTodo}
-      </button>
-      <button onClick={handleShowDoneTasks} className={activeButton === "done" ? "active-button" : ""} title="Show completed tasks">
-        {translations.showDone}
-      </button>
-      <button onClick={handleShowAllTasks} className={activeButton === "all" ? "active-button" : ""} title="Show all tasks">
-        {translations.showAll}
-      </button>
-      <button onClick={handleDeleteAllCheckedTask} title="Delete all completed tasks">
-        {translations.deleteAll}
-      </button>
+    <>
+      <div className="footer-buttons">
+        <button onClick={handleShowTodoTasks} className={activeButton === "todo" ? "active-button" : ""} title="Show tasks to do">
+          {translations.showTodo}
+        </button>
+        <button onClick={handleShowDoneTasks} className={activeButton === "done" ? "active-button" : ""} title="Show completed tasks">
+          {translations.showDone}
+        </button>
+        <button onClick={handleShowAllTasks} className={activeButton === "all" ? "active-button" : ""} title="Show all tasks">
+          {translations.showAll}
+        </button>
+        <button onClick={handleDeleteAllCheckedTask} title="Delete all completed tasks">
+          {translations.deleteAll}
+        </button>
+      </div>
       <p className="tasksLeft">{tasksLeftCounter > 0 ? (tasksLeftCounter > 1 ? tasksLeftCounter + translations.tasksLeftPlural : tasksLeftCounter + translations.tasksLeft) : ""}</p>
-    </div>
+    </>
   );
 };
 
